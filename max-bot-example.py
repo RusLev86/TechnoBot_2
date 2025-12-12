7b0bfb1830f69c6d34137e9febdb2ab7030d0f37
RusLev86 <ruslev@gmail.com>
2025-12-12 14:03:02 +0500

    Update max-bot-example.py

--- max-bot-example.py
+++ max-bot-example.py
@@ -9,3 +9,27 @@ bot.remove_webhook()
 
 # Запускаем опрос
 bot.polling(none_stop=True)
+
+import asyncio
+import logging
+
+from maxapi import Bot, Dispatcher, F
+from maxapi.types import MessageCreated
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot()
+dp = Dispatcher()
+
+
+@dp.message_created(F.message.body.text)
+async def echo(event: MessageCreated):
+    await event.message.answer(f"Повторяю за вами: {event.message.body.text}")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
