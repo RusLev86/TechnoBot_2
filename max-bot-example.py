175a44a4312d7135c5b8e9c7e147e34c48132545
RusLev86 <ruslev@gmail.com>
2025-12-12 14:26:21 +0500

    Update max-bot-example.py

--- max-bot-example.py
+++ max-bot-example.py
@@ -1,7 +1,7 @@
 import telebot
 
 # Вставьте свой токен
-TOKEN = 'f9LHodD0cOKL_H-R_qDhhjRvFlEohd8CFd37GA1_9Z_0e1oY5DT1sbd5IMUFPTO-LQ4rzxP50anS1eaeZBiG'
+TOKEN = ''
 bot = telebot.TeleBot(TOKEN)
 
 # Удаляем вебхук
